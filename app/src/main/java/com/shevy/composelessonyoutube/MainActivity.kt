f5a21dbe455e4a2b2d3e58a8be4dc292cadc0795
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-08-05 14:56:16 +0700

    Lesson 4. States

--- app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
@@ -1,119 +1,61 @@
 package com.shevy.composelessonyoutube
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
-                ListItem("Dmitriy Shev", "Engineer")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircleItem()
             }
         }
     }
 
     @Composable
-    private fun ListItem(name: String, prof: String) {
-        Card(
+    private fun CircleItem() {
+        val counter = remember { mutableStateOf(0) }
+        val color = remember { mutableStateOf(Color.Blue) }
+
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-                .pointerInput(Unit){
-                    detectDragGesturesAfterLongPress { change, dragAmount ->
-                        Log.d("TestLog", "Long press: $dragAmount")
-                    }
-                }
+                .size(100.dp)
+                .background(color = color.value, shape = CircleShape)
                 .clickable {
-                    Log.d("TestLog", "Clicked")
-                },
-            shape = RoundedCornerShape(15.dp),
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 5.dp
-            )
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.photo),
-                            contentDescription = "image",
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                //.padding(5.dp)
-                                .size(64.dp)
-                            //.clip(CircleShape)
-                        )
-                        Column(
-                            modifier = Modifier.padding(start = 16.dp)
-                        ) {
-                            Text(text = name)
-                            Text(text = prof)
-                        }
+                    when (++counter.value) {
+                        10 -> color.value = Color.Red
+                        20 -> color.value = Color.Green
                     }
-                }
-                Box(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.CenterEnd
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .background(Color.Blue)
-                            .height(64.dp)
-                            .width(20.dp),
-                        verticalArrangement = Arrangement.SpaceBetween,
-                        horizontalAlignment = Alignment.End
-                    ) { }
-                }
-            }
+                },
+            contentAlignment = Alignment.Center
+        ) {
+
+            Text(
+                text = counter.value.toString(),
+                style = TextStyle(color = Color.White, fontSize = 20.sp)
+            )
         }
     }
 }
