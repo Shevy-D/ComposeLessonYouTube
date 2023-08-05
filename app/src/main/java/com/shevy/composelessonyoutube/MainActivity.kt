e43e9dabbdbe0cfe02756fa7c5f586be52a1f179
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-08-05 14:21:23 +0700

    Lesson 3

--- app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.shevy.composelessonyoutube
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,7 +20,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -22,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -30,7 +39,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                ListItem("Dmitriy Shev", "Engineer")
+                ListItem("Dmitriy Shev", "Engineer")
+                ListItem("Dmitriy Shev", "Engineer")
+                ListItem("Dmitriy Shev", "Engineer")
+                ListItem("Dmitriy Shev", "Engineer")
                 ListItem("Dmitriy Shev", "Engineer")
                 ListItem("Dmitriy Shev", "Engineer")
                 ListItem("Dmitriy Shev", "Engineer")
@@ -46,7 +60,15 @@ class MainActivity : ComponentActivity() {
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(10.dp),
+                .padding(10.dp)
+                .pointerInput(Unit){
+                    detectDragGesturesAfterLongPress { change, dragAmount ->
+                        Log.d("TestLog", "Long press: $dragAmount")
+                    }
+                }
+                .clickable {
+                    Log.d("TestLog", "Clicked")
+                },
             shape = RoundedCornerShape(15.dp),
             elevation = CardDefaults.cardElevation(
                 defaultElevation = 5.dp
