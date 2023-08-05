e046011f782b9a9ed41660f49771bf6de5c0428f
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-08-05 11:15:01 +0700

    Lesson 1

--- app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
@@ -3,44 +3,49 @@ package com.shevy.composelessonyoutube
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.shevy.composelessonyoutube.ui.theme.ComposeLessonYouTubeTheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeLessonYouTubeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .background(Color.Blue),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Text(color = Color.White, text = "Hello!")
+                    Text(color = Color.White, text = "Android!")
+                    Text(color = Color.White, text = "Apple!")
+                }
+
+                Column(
+                    modifier = Modifier
+                        .background(Color.Yellow)
+                        .fillMaxHeight(0.5f),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(color = Color.Black, text = "Hello!")
+                    Text(color = Color.Black, text = "Android!")
+                    Text(color = Color.Black, text = "Apple!")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeLessonYouTubeTheme {
-        Greeting("Android")
     }
 }
