4157995f59ba201f571d2f0182cd4923177c871a
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-08-05 12:11:33 +0700

    Lesson 2

--- app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
@@ -3,49 +3,96 @@ package com.shevy.composelessonyoutube
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column() {
+                ListItem("Dmitriy Shev", "Engineer")
+                ListItem("Dmitriy Shev", "Engineer")
+                ListItem("Dmitriy Shev", "Engineer")
+                ListItem("Dmitriy Shev", "Engineer")
+                ListItem("Dmitriy Shev", "Engineer")
+                ListItem("Dmitriy Shev", "Engineer")
+            }
+        }
+    }
+
+    @Composable
+    private fun ListItem(name: String, prof: String) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            shape = RoundedCornerShape(15.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 5.dp
+            )
+        ) {
             Row(
-                modifier = Modifier.fillMaxSize(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-
-                Column(
-                    modifier = Modifier
-                        .background(Color.Blue),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(color = Color.White, text = "Hello!")
-                    Text(color = Color.White, text = "Android!")
-                    Text(color = Color.White, text = "Apple!")
+                Box {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.photo),
+                            contentDescription = "image",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                //.padding(5.dp)
+                                .size(64.dp)
+                            //.clip(CircleShape)
+                        )
+                        Column(
+                            modifier = Modifier.padding(start = 16.dp)
+                        ) {
+                            Text(text = name)
+                            Text(text = prof)
+                        }
+                    }
                 }
-
-                Column(
-                    modifier = Modifier
-                        .background(Color.Yellow)
-                        .fillMaxHeight(0.5f),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.CenterEnd
                 ) {
-                    Text(color = Color.Black, text = "Hello!")
-                    Text(color = Color.Black, text = "Android!")
-                    Text(color = Color.Black, text = "Apple!")
+                    Column(
+                        modifier = Modifier
+                            .background(Color.Blue)
+                            .height(64.dp)
+                            .width(20.dp),
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment = Alignment.End
+                    ) { }
                 }
             }
         }
     }
 }
+
