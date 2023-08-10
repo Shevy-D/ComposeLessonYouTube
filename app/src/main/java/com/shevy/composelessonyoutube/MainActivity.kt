f03419478b9fdf047dabe3545d8251ae169a0ce5
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-08-10 21:22:31 +0700

    Lesson 8. The Forecast app

--- app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
@@ -1,51 +1,103 @@
 package com.shevy.composelessonyoutube
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.shevy.composelessonyoutube.ui.theme.Green80
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.shevy.composelessonyoutube.ui.theme.ComposeLessonYouTubeTheme
+import org.json.JSONObject
+
+const val API_KEY = "012c748818fd41bbbdf112239221805"
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Green80)
-            ) {
-                itemsIndexed(
-                    listOf(
-                        ItemRowModel(R.drawable.image_1, "Image 1", "Test sdjbdsakjdsbkjadsbdcbbjcabjcd sdbcjcbbkjdjkcd ckbjcabjkdbjdbjksdbjksd cbsakjbdbjkds" +
-                                "cacbdabdsakbjdsjkdkjdscjkdkjdajkdjnkdnjdjadnsjk jdsanckjndackla"),
-                        ItemRowModel(R.drawable.image_2, "Image 2", "Test"),
-                        ItemRowModel(R.drawable.image_3, "Image 3", "Test"),
-                        ItemRowModel(R.drawable.image_4, "Image 4", "Test"),
-                        ItemRowModel(R.drawable.image_5, "Image 5", "Test"),
-                        ItemRowModel(R.drawable.image_6, "Image 6", "Test")
-                    )
-                ) { _, item ->
-                    MyRow(item = item)
+            ComposeLessonYouTubeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("London", this@MainActivity)
                 }
             }
         }
     }
+
+    @Composable
+    fun Greeting(name: String, context: Context) {
+        val state = remember {
+            mutableStateOf("Unknown")
+        }
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Temp in $name = ${state.value} C")
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Button(
+                    onClick = { getResult(name, state, context) }, modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(text = "Refresh")
+                }
+            }
+        }
+    }
+
+}
+
+private fun getResult(city: String, state: MutableState<String>, context: Context) {
+    val url = "https://api.weather.com/v1/current.json" +
+            "?key=$API_KEY&" +
+            "q=$city" +
+            "&aqi=no"
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        { response ->
+            val obj = JSONObject(response)
+            state.value = obj.getJSONObject("current").getString("temp_c")
+        },
+        { error ->
+            Log.d("MyLog", "Error = $error")
+        }
+    )
+
+    queue.add(stringRequest)
 }
 
