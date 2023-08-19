5e32a1ae64002de85746eb922f3cc4963ac7381e
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-08-19 19:59:28 +0700

    Lesson №21. Scaffold и TopAppBar в Jetpack Compose

--- app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.shevy.composelessonyoutube
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.shevy.composelessonyoutube.ui.RuleScreen
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import com.shevy.composelessonyoutube.ui.theme.ComposeLessonYouTubeTheme
-import com.shevy.composelessonyoutube.ui.theme.GreenBg
 
 class MainActivity : ComponentActivity() {
 
@@ -16,14 +30,60 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLessonYouTubeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = GreenBg
-                ) {
-                    RuleScreen()
-                }
+                MainScreen(applicationContext)
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen(context: Context) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Menu")
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Yellow),
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            Toast.makeText(context, "Menu", Toast.LENGTH_SHORT).show()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Menu"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            Toast.makeText(context, "Delete", Toast.LENGTH_SHORT).show()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = "Delete"
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            Toast.makeText(context, "Share", Toast.LENGTH_SHORT).show()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Share,
+                            contentDescription = "Share"
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+
     }
 }
