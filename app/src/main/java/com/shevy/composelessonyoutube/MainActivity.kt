a9abe5d168e20f874485ee2ec2d23cb784d80cc5
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-08-22 18:52:54 +0700

    Lesson. Search Bar

--- app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/shevy/composelessonyoutube/MainActivity.kt
@@ -1,123 +1,41 @@
 package com.shevy.composelessonyoutube
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Snackbar
-import androidx.compose.material.SnackbarHost
-import androidx.compose.material.SnackbarResult
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Share
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.unit.dp
-import com.shevy.composelessonyoutube.ui.DrawerBody
-import com.shevy.composelessonyoutube.ui.DrawerHeader
 import com.shevy.composelessonyoutube.ui.theme.ComposeLessonYouTubeTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val searchText = remember { mutableStateOf("") }
             ComposeLessonYouTubeTheme {
-                MainScreen(applicationContext)
+                SearchBar(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    query = searchText.value,
+                    onQueryChange = { text ->
+                        searchText.value = text
+                    },
+                    onSearch = { },
+                    placeholder = { Text(text = "Search...") },
+                    active = false,
+                    onActiveChange = { }
+                ) { }
             }
         }
-    }
-}
-
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@Composable
-fun MainScreen(context: Context) {
-    val coroutineScope = rememberCoroutineScope()
-    val scaffoldState = rememberScaffoldState()
-
-    Scaffold(
-        scaffoldState = scaffoldState,
-        snackbarHost = { host ->
-            SnackbarHost(hostState = host) { data ->
-                Snackbar(
-                    backgroundColor = Color.White,
-                    snackbarData = data,
-                    shape = RoundedCornerShape(20.dp),
-                    contentColor = Color.Green,
-                    modifier = Modifier.padding(bottom = 100.dp)
-                )
-            }
-        },
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "Menu")
-                },
-                backgroundColor = Color.White,
-                navigationIcon = {
-                    IconButton(
-                        onClick = {
-                            coroutineScope.launch {
-                                scaffoldState.drawerState.open()
-                            }
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Menu"
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(
-                        onClick = {
-                            coroutineScope.launch {
-                                val result = scaffoldState.snackbarHostState.showSnackbar(
-                                    message = "Item deleted!",
-                                    actionLabel = "Undone"
-                                )
-                                if (result == SnackbarResult.ActionPerformed) {
-                                    Toast.makeText(context, "Item recovered", Toast.LENGTH_SHORT)
-                                        .show()
-                                }
-                            }
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.Delete,
-                            contentDescription = "Delete"
-                        )
-                    }
-                    IconButton(
-                        onClick = { Toast.makeText(context, "Share", Toast.LENGTH_SHORT).show() }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.Share,
-                            contentDescription = "Share"
-                        )
-                    }
-                }
-            )
-        },
-        drawerContent = {
-            DrawerHeader()
-            DrawerBody()
-        }
-    ) {
     }
 }
